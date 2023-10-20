73b2dd04a9d9072ee86763e2cac433db9d79efb7
JohnMigwi <johnmigwi101@gmail.com>
2023-10-20 21:11:22 +0300

    Hex color

--- javascript-basic-projects-master/01-color-flipper/setup/app.js
+++ javascript-basic-projects-master/01-color-flipper/setup/app.js
@@ -1,10 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025","indigo","gold","dark black"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
 const getRandomNumber = () =>Math.floor(Math.random()*colors.length);
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', () =>{
     const randomNumber = getRandomNumber();
     console.log(randomNumber);
     document.body.style.backgroundColor = colors [randomNumber];
--- javascript-basic-projects-master/01-color-flipper/setup/hex.js
+++ javascript-basic-projects-master/01-color-flipper/setup/hex.js
@@ -1,1 +1,21 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const color = document.querySelector(".color");
+const btn = document.getElementById('btn');
+const items = 6;
+
+const getRandomColors = () =>{
+    const randomItems = [];
+    for(let i=0; i<items; i++) {
+        const randos = Math.floor(Math.random() *hex.length);
+        randomItems.push(hex[randos]);
+    }
+    return randomItems.join('');
+}
+
+
+btn.addEventListener('click', () => {
+    const randomItems = getRandomColors();
+    console.log(randomItems);
+    document.body.style.backgroundColor = '#' + randomItems;
+    color.textContent = randomItems;
+})
